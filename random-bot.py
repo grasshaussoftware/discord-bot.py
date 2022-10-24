f0a18ec65e6bdd7f44accad5ee1344758e657ec7
deusopus <deusopus@gmail.com>
2022-10-24 07:17:53 -0700

    Update random-bot.py

--- random-bot.py
+++ random-bot.py
@@ -1,4 +1,4 @@
-#discord.py bot to return a random number between 1-10 in the channel
+#discord.py bot to return a random number between 1 and 10, answer a "yes or no" question, and roll a six sided die
 
 import discord
 import random
@@ -7,8 +7,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
-
+yes_no = ["yes", "no"]
+dice = ["[ . ]", "[ : ]", "[ .: ]", "[ :: ]", "[ :.: ]", "[ ::: ]"]
 
 @client.event
 async def on_ready():
@@ -18,7 +18,13 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if 'random' in message.content:
-        await message.send(random.randint(1, 10))
+    if message.channel.id != 1018756543816155187:
+        return
+    if 'random' in message.content.lower():
+        await message.channel.send(random.randint(1, 10))
+    if 'yes or no' in message.content.lower():
+        await message.channel.send(random.choice(yes_no))
+    if 'roll' in message.content.lower():
+        await message.channel.send(random.choice(dice))
 
 client.run('<private token>')
